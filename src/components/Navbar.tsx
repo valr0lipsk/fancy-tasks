3d801b5483d19fdb369af2147180593bed8725f6
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2022-11-30 12:41:22 +0300

    Implement Navbar component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import { ReactComponent as HomeLogo } from "../assets/home.svg";
+import { ReactComponent as LinesLogo } from "../assets/lines.svg";
+import { ReactComponent as ExitLogo } from "../assets/exit.svg";
 
 const Navbar = () => {
   return (
-    <div className="nav">
+    <nav className="nav">
       <img src={logo} alt="" className="nav__logo" />
-    </div>
+      <div className="nav__top-icons">
+        <a className="nav__button">
+          <HomeLogo />
+        </a>
+        <a className="nav__button">
+          <LinesLogo />
+        </a>
+      </div>
+      <a className="nav__button nav__button--bottom">
+        <ExitLogo />
+      </a>
+    </nav>
   );
 };
 
