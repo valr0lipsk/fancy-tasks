40b03c0e12adfbb69fc466b52c55ffdad0945040
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2022-12-01 15:14:15 +0300

    Implement TasksList components.

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task";
 
-import tasks from "../assets/tasks.json";
+import tasksList from "../assets/tasks.json";
 
 const TasksList = () => {
+  const [tasks, setTasks] = useState(tasksList);
+  const [filter, setFilter] = useState("All");
+
+  const filterHandler = (value: string) => {
+    setFilter(value);
+  };
+
+  useEffect(() => {
+    filter !== "All"
+      ? setTasks(tasksList.filter((e) => e.status === filter))
+      : setTasks(tasksList);
+  }, [filter]);
+
   return (
-    <ul>
-      {tasks.map((e) => (
-        <Task {...e} />
-      ))}
-    </ul>
+    <>
+      <p className="title title--main">Tasks</p>
+      <div className="filters">
+        <button
+          className={"btn--text " + (filter === "All" ? "active" : "")}
+          onClick={() => filterHandler("All")}
+        >
+          All
+        </button>
+        <button
+          className={"btn--text " + (filter === "To do" ? "active" : "")}
+          onClick={() => filterHandler("To do")}
+        >
+          To do
+        </button>
+        <button
+          className={"btn--text " + (filter === "In progress" ? "active" : "")}
+          onClick={() => filterHandler("In progress")}
+        >
+          In progress
+        </button>
+        <button
+          className={"btn--text " + (filter === "Done" ? "active" : "")}
+          onClick={() => filterHandler("Done")}
+        >
+          Done
+        </button>
+      </div>
+      <ul className="list">
+        {tasks.map((e) => (
+          <Task key={e.id} {...e} />
+        ))}
+      </ul>
+    </>
   );
 };
 
