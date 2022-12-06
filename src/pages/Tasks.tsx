21c9cac59a7fb9d4f27214cfbbb4900a48d58232
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2022-12-06 11:08:14 +0300

    Implement InputBlock.

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import InputBlock from "../components/InputBlock";
 import Navbar from "../components/Navbar";
 
 const Tasks = () => {
   return (
     <div className="main main--tasks">
       <Navbar />
+      <div className="tasks__container">
+        <InputBlock />
+      </div>
     </div>
   );
 };
